4c59f1e4af6b986655d101b2f9dd409d5003ceb2
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2024-11-27 16:39:51 -0600

    fix error when no episode found

--- tvshow/cron.py
+++ tvshow/cron.py
@@ -7,12 +7,15 @@ from tvshow.utils.tvdb_api_wrap import get_series_with_id
 def update_db_cron():
     show_list = Show.objects.filter(Q(runningStatus='Continuing'),Q(last_updated__lte=timezone.now()-timedelta(days=4)))
     for show in show_list:
-        flag = show.update_show_data("0")
-        show.last_updated = timezone.now()
-        show_data = get_series_with_id(int(show.tvdbID))
-        show.network = show_data['latestNetwork']['name']
-        show.save()
-        if flag:
-            print('%s has been updated.'%show.seriesName)
+        try:
+            flag = show.update_show_data("0")
+            show.last_updated = timezone.now()
+            show_data = get_series_with_id(int(show.tvdbID))
+            show.network = show_data['latestNetwork']['name']
+            show.save()
+            if flag:
+                print('%s has been updated.'%show.seriesName)
+        except:
+            print('%s had errors while updating.'%show.seriesName)
     
     print('Completed db update')
