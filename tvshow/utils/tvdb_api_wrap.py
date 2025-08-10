83a825eae66f6eb9fa36ad04a8a10e4d94eb0896
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-08-10 01:41:17 -0500

    fix api/token stuff

--- tvshow/utils/tvdb_api_wrap.py
+++ tvshow/utils/tvdb_api_wrap.py
@@ -1,31 +1,55 @@
-import requests
-import json
-from django.utils import six
-import os,time
-from django.conf import settings
-from datetime import datetime
+import time
 import tvdb_v4_official
+from django.conf import settings
 
-if six.PY2:
-	from urllib import quote
-else:
-	from urllib.parse import quote
+# --- Client Caching Mechanism ---
+# We will store the client instance and when its token was generated.
+_tvdb_client = None
+_client_creation_time = 0
+# TVDB tokens last 24 hours. We'll refresh after 23.5 hours to be safe.
+TOKEN_LIFESPAN_SECONDS = 23.5 * 60 * 60
 
-tvdb = tvdb_v4_official.TVDB("67322ef0-b51d-41b2-8af6-fcc3d07b1c1e")
+def get_tvdb_client():
+	"""
+	Returns a valid TVDB client instance.
+
+	It creates a new client if the existing one is missing or its
+	token is nearing expiration.
+	"""
+	global _tvdb_client, _client_creation_time
+
+	# Check if the client is invalid or its token is old
+	is_client_invalid = not _tvdb_client
+	is_token_expired = (time.time() - _client_creation_time) > TOKEN_LIFESPAN_SECONDS
+
+	if is_client_invalid or is_token_expired:
+		print("Creating a new TVDB client instance...")
+		# Initialize the client. This automatically performs login and gets a new token.
+		_tvdb_client = tvdb_v4_official.TVDB("67322ef0-b51d-41b2-8af6-fcc3d07b1c1e")
+		_client_creation_time = time.time()
+
+	return _tvdb_client
+
+# --- Refactor Your Functions to Use the Factory ---
 
 def search_series_list(series_name):
+	tvdb = get_tvdb_client() 
 	return tvdb.search(series_name,type="series")
 
 def search_movie_list(movie_name):
+	tvdb = get_tvdb_client()
 	return tvdb.search(movie_name,type="movie")
 
 def get_series_with_id(tvdbID):
+	tvdb = get_tvdb_client()
 	return tvdb.get_series_extended(tvdbID)
 
 def get_movie_with_id(tvdbID):
+	tvdb = get_tvdb_client()
 	return tvdb.get_movie_extended(tvdbID)
 
 def get_season_episode_list(tvdbID, number):
+	tvdb = get_tvdb_client()
 	series = tvdb.get_series_extended(tvdbID)
 	for season in sorted(series["seasons"], key=lambda x: (x["type"]["name"], x["number"])):
 		if season["type"]["name"] == "Aired Order" and season["number"] == number:
@@ -46,24 +70,30 @@ def get_all_episodes(tvdbID,start_season):
 	return show
 
 def get_series_translation(tvdbID,language):
+	tvdb = get_tvdb_client()
 	return tvdb.get_series_translation(tvdbID,language)
 
 def get_episode_translation(tvdb_epid,language):
+	tvdb = get_tvdb_client()
 	return tvdb.get_episode_translation(tvdb_epid,language)
 
 def get_episode(episodeID):
+	tvdb = get_tvdb_client()
 	return tvdb.get_episode(episodeID)
 
 def get_series(tvdbID):
+	tvdb = get_tvdb_client()
 	return tvdb.get_series(tvdbID)
 
 def get_image_link(tvdbID):
+	tvdb = get_tvdb_client()
 	series = tvdb.get_series_artworks(tvdbID, 'en')
 	image = series['image']
 	#print(image)
 	return image
 
 def get_image_from_search(series_name):
+	tvdb = get_tvdb_client()
 	query = tvdb.search(series_name,type="series")
 	image_link = query[0]['image_url']
 	overview = query[0]['overview']
